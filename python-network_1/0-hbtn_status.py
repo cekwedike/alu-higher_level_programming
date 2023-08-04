b6bcdc746e45f62bf959d54122ade04642a715d6
cekwedike <c.ekwedike@alustudent.com>
2023-08-04 09:55:19 -0700

    Corrections

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -19,13 +19,16 @@ import urllib.request
 
 if __name__ == "__main__":
     '''
-    Something here
+    This script fetches the status information from different URLs and
+    prints the results in a formatted way.
     '''
-    url = 'http://0.0.0.0:5050/status'
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        data = response.read()
-        info = f"Body response:\n\t- type: {type(data)}\n\t- \
+    urls = ['https://alu-intranet.hbtn.io/status', 'http://0.0.0.0:5050/status']
+
+    for url in urls:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            data = response.read()
+            info = f"Body response for {url}:\n\t- type: {type(data)}\n\t- \
 content: {data}\n\t- utf8 content: {data.decode('utf-8')}"
-        print(info)
+            print(info)
 
