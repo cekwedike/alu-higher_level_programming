c7b9def08b3b9b78305d6b20e0ed4e9496c37572
cekwedike <c.ekwedike@alustudent.com>
2023-08-04 09:35:02 -0700

    Catch Extra Task

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# catch_me using curl
-curl -s -X GET 0.0.0.0:5000/catch_me
+# This script sends a request to 0.0.0.0:5000/catch_me using curl
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "You got me!"
 
