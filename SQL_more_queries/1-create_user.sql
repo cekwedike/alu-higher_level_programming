43e052e8774897cc84b76e16a4e34d1a7bfb6a85
cekwedike <c.ekwedike@alustudent.com>
2023-08-04 09:05:38 -0700

    corrections

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Creates user user_0d_1 with password user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- All SQL keywords should be in uppercase
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
