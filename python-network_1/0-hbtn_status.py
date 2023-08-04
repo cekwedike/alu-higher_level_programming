4d3c44e6e0a416c34a42fab01505ab59a984e814
cekwedike <c.ekwedike@alustudent.com>
2023-08-04 09:52:56 -0700

    Corrections

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 '''
-This script fetches the status information from the URL
- "https://alu-intranet.hbtn.io/status" and prints the results in a formatted way.
-
-Dependencies:
-- requests library (install using "pip install requests")
+This script fetches the status information from a URL and prints the results
+in a formatted way.
 
 Usage:
-python fetch_status.py
+    python fetch_status.py
+
+Dependencies:
+    - requests library (install using "pip install requests")
 
 Example Output:
+    Body response:
+        - type: <class 'bytes'>
+        - content: b'OK\n'
+        - utf8 content: OK
 '''
 import urllib.request
 
@@ -17,10 +21,11 @@ if __name__ == "__main__":
     '''
     Something here
     '''
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'http://0.0.0.0:5050/status'
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         data = response.read()
         info = f"Body response:\n\t- type: {type(data)}\n\t- \
 content: {data}\n\t- utf8 content: {data.decode('utf-8')}"
         print(info)
+
